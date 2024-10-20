b6b47fb92de5ff91d6d41b5602082208404d1878
Hukumraj Singh Deora <me@hukumraj-singh-deora.party>
2024-10-21 01:17:15 +0200

    fix: unit test

--- internal/service/moduleconfig/reader/moduleconfig_reader_test.go
+++ internal/service/moduleconfig/reader/moduleconfig_reader_test.go
@@ -373,6 +373,13 @@ var expectedReturnedModuleConfig = contentprovider.ModuleConfig{
 	Beta:          false,
 	Labels:        map[string]string{"label1": "value1"},
 	Annotations:   map[string]string{"annotation1": "value1"},
+	AssociatedResources: []*metav1.GroupVersionKind{
+		{
+			Group:   "networking.istio.io",
+			Version: "v1alpha3",
+			Kind:    "Gateway",
+		},
+	},
 	Manager: &contentprovider.Manager{
 		Name:      "manager-name",
 		Namespace: "manager-namespace",
