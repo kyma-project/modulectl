ab6a91a734e1e49dec19c40aa121db667a0eb8fb
medmes <mohammed.mesaoudi@sap.com>
2024-11-28 16:03:10 +0100

    reformat the error message

--- internal/service/contentprovider/moduleconfig.go
+++ internal/service/contentprovider/moduleconfig.go
@@ -54,13 +54,13 @@ func (s *ModuleConfigProvider) validateArgs(args types.KeyValueArgs) error {
 	if value, ok := args[ArgModuleName]; !ok {
 		return fmt.Errorf("%s: %w", ArgModuleName, ErrMissingArg)
 	} else if value == "" {
-		return fmt.Errorf("must not be empty: %s: %w", ArgModuleName, ErrInvalidArg)
+		return fmt.Errorf("%s must not be empty: %w", ArgModuleName, ErrInvalidArg)
 	}
 
 	if value, ok := args[ArgModuleVersion]; !ok {
 		return fmt.Errorf("%s: %w", ArgModuleVersion, ErrMissingArg)
 	} else if value == "" {
-		return fmt.Errorf("must not be empty: %s: %w", ArgModuleVersion, ErrInvalidArg)
+		return fmt.Errorf("%s must not be empty: %w", ArgModuleVersion, ErrInvalidArg)
 	}
 
 	return nil
