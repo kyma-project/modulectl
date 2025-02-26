316fe19f99f82610f12ed5fa7272782324777c84
Christoph Schwägerl <c.schwaegerl@sap.com>
2025-02-26 10:04:13 +0100

    check mandatory arguments

--- scripts/run-e2e-test.sh
+++ scripts/run-e2e-test.sh
@@ -6,10 +6,19 @@ cd $(git rev-parse --show-toplevel)
 while [ $# -gt 0 ]; do
   case "$1" in
     --cmd=*)
-      cmd="${1#*=}"
+      CMD="${1#*=}"
       ;;
   esac
   shift
 done
 
-export PATH=$(pwd)/bin/:$PATH && make -C ./tests/e2e test-${cmd}-cmd
+# Check for mandatory arguments
+if [[ -z "$CMD" ]]; then
+  echo "[$(basename $0)] Missing required arguments"
+  echo "Usage: $(basename $0) --cmd=<create|scaffold>"
+  exit 1
+fi
+
+
+
+export PATH=$(pwd)/bin/:$PATH && make -C ./tests/e2e test-${CMD}-cmd
